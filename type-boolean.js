718f47754e0f7efc9568e3ca5000bb7fa1781984
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-08 17:05:48 -0300

    Tipo Boolean

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//boolean
+//true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 10;
+const segundonumero = 10;
+let cadasTroAtivado = true;
+
+console.log(primeiroNumero === segundonumero)
+
+const texto1 = "JavaScript";
+const texto2 = "javascript";
+
+console.log(texto1 === texto2)
