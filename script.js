7f9fc40080a15343e0ee91645eac8e33fa353576
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-08 22:49:01 -0300

    Três formas de escrever variáveis: var, let e const

---
+++ script.js
@@ -0,0 +1,35 @@
+// var
+
+// var altura = 5;
+// var comprimento =7;
+// var area;
+
+// var area = altura * comprimento;
+// console.log(area)
+
+// let forma = 'quadrado';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'quadrado') {
+//   area = altura * comprimento;
+// }else {
+//   area = altura * comprimento /2;
+// }
+
+// console.log(area)
+
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+
+if (forma === 'quadrado') {
+  area = altura * comprimento;
+}else {
+  area = altura * comprimento /2
+}
+
+
+console.log(area)
