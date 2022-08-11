17ec8a85d4943d59d5e6447892175bac803d63d0
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-10 21:39:23 -0300

    Conversões de números e booleanos de Strings

---
+++ conversao1-valores.js
@@ -0,0 +1,16 @@
+ // String
+ 
+ let telefone = 23452345;
+ console.log("O telefone é " + telefone);
+
+ let telefone1 = 23452345;
+ console.log("O telefone é " + String(telefone))
+ 
+ let telefone2 = 23452345;
+ console.log("O telefone é " + telefone.toString())
+
+ let usuarioConectado = false;
+ console.log(String(usuarioConectado))
+
+ let usuarioConectado1 = true;
+ console.log(String(usuarioConectado1))
