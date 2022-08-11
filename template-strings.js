783c5e3308ebf32ee2bc4022f3abda2d141b993c
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-11 20:56:57 -0300

    Template literal

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Rose";
+const idade = 2022-1989;
+const cidadeDeNasci = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", tenho " + idade + 
+// " anos e nasci na cidade de " + cidadeDeNasci;
+
+
+const apresentacao = `Meu nome é ${nome} , tenho ${idade} anos e nasci na cidade de ${cidadeDeNasci}`;
+console.log(apresentacao);
