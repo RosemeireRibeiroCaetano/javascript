c0106e769ff8472f3b1431231839b187faf9e613
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-12 22:56:43 -0300

    Deletando elementos

---
+++ arrays2.js
@@ -0,0 +1,16 @@
+const numeros = [ 10, 11, 12, 13,14]
+numeros.pop()
+console.log(numeros)
+
+const texto = ["arroz", "feijao", "carne", "salada"]
+texto.pop()
+console.log(texto)
+
+// Media escolar
+const notas = [10, 6.5, 8, 5.5, 10]
+notas.pop()
+console.log(notas)
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3])/ notas.length
+console.log(`A média é ${media}`)
+
