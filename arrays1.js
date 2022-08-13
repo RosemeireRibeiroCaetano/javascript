d4078d9d3a7ac68df8fdb9a02c8b8ed90c3478bb
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-12 22:27:07 -0300

    Adicionando elementos

---
+++ arrays1.js
@@ -0,0 +1,16 @@
+const numeros = [10, 11, 12]
+numeros.push(13)
+// console.log(numeros)
+
+numeros.push(14, 15, 16)
+// console.log(numeros)
+
+numeros.push(17, 18, 19, 20)
+console.log(numeros)
+
+const texto = ["banana", "maça", "laranja"]
+texto.push('morango')
+console.log(texto)
+
+texto.push("cereja", "figo", "goiaba", "melão")
+console.log(texto)
