6038e9a8a5e65b4039aec48206b4f14500efe3ae
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-14 14:55:49 -0300

    Adicionando e removendo elementos

---
+++ arrays.js
@@ -0,0 +1,10 @@
+const jantaDeHoje = ['arroz', 'feijão', 'carne']
+jantaDeHoje.pop()
+jantaDeHoje.pop()
+
+
+jantaDeHoje.push('salada')
+jantaDeHoje.push('ovo')
+jantaDeHoje.push('frango')
+
+console.log(jantaDeHoje)
