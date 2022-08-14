27c58197e6de12096499a44bbe4d89183ebc0240
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-14 20:09:05 -0300

    Concatenando arrays

---
+++ arrays5.js
@@ -0,0 +1,6 @@
+const salaDeJava = ['Rosa', 'Sandra', 'Adriana','Carlos']
+const salaDeJavaScript = ['Amanda', 'Pedro', 'Heloísa', 'Alice']
+
+const salasUnificadas = salaDeJava.concat(salaDeJavaScript)
+
+console.log(salasUnificadas)
