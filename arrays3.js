5dc54b926acab603f2d59dbd7cbf4dd9b275c111
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-14 19:49:56 -0300

    Alterando com splice

--- arrays3.js
+++ arrays3.js
@@ -1,9 +1,9 @@
 const nomes = ['Miguel', 'Samuel', 'Helena', 'Sofhia', 'Andre', 'Valentina'
-, 'Bernardo', 'Maria', 'Paulo', 'Laura', 'Gabriel', 'Fabiana', 
-'lara', 'Theo', 'Alice', 'Manuela']
-
-const turma1 = nomes.slice(0,nomes.length/2)
-const turma2 = nomes.slice(nomes.length/2)
-
-console.log(`Alunos da turma 1: ${turma1}`)
-console.log(`Alunos da turma 2: ${turma2}`)
+ , 'Bernardo', 'Maria', 'Paulo', 'Laura', 'Gabriel', 'Fabiana', 
+ 'lara', 'Theo', 'Alice', 'Manuela']
+ 
+ const turma1 = nomes.slice(0,nomes.length/2)
+ const turma2 = nomes.slice(nomes.length/2)
+ 
+ console.log(`Alunos da turma 1: ${turma1}`)
+ console.log(`Alunos da turma 2: ${turma2}`) 
---
+++ arrays4.js
@@ -0,0 +1,7 @@
+const nomes = ['Miguel', 'Sofhia', 'Eduarda'
+, 'Helena', 'Lucas', 'Gael']
+
+// nomes.splice(1,2, "Theo")
+nomes.splice(0,2, "Theo")
+
+console.log(nomes)
