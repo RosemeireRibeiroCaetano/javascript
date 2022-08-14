3e41eb11fbda79855c3fe636e1bdfc38d65ddfd4
Rosemeire Ribeiro <rosemeireribeirobraz54@gmail.com>
2022-08-14 20:50:18 -0300

    Criando lista com 2 dimensões

---
+++ arrays6.js
@@ -0,0 +1,9 @@
+const lista = ['Olívia', 'Manuela', 'Luiza', 'Greta']
+
+const media = [10,7,9,6]
+
+const listaDeNotas = [lista,media]
+
+console.log(`${listaDeNotas[0] [3]}, sua media é ${listaDeNotas[1][02]}`)
+
+
